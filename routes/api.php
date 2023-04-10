bab5e2fd3d4c02bc4de0616e1fd0bb9e1a4a38ce
IIndave <inigoindave@gmail.com>
2023-04-10 10:47:34 -0700

    create VentasController

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/tipocliente', 'App\Http\Controllers\ApiTipoClienteController@buscar');
+Route::get('/ventas', 'App\Http\Controllers\VentasController@buscar');
+
